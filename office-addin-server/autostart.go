df5d808cdf5b95d7195e70437fbc6d3df1f48574
qhsmhq <qin__xuan@yeah.net>
2026-05-26 19:38:21 +0800

    refactor: use syscall for Windows registry in autostart

--- office-addin-server/autostart.go
+++ office-addin-server/autostart.go
@@ -6,42 +6,38 @@ import (
 	"unsafe"
 )
 
-const (
-	regKeyPath = `Software\Microsoft\Windows\CurrentVersion\Run`
-)
-
 var (
-	advapi32       = syscall.NewLazyDLL("advapi32.dll")
-	regOpenKeyExW  = advapi32.NewProc("RegOpenKeyExW")
-	regSetValueExW = advapi32.NewProc("RegSetValueExW")
-	regCloseKey    = advapi32.NewProc("RegCloseKey")
+	advapi32 = syscall.MustLoadDLL("advapi32.dll")
+	regOpenKeyExW  = advapi32.MustFindProc("RegOpenKeyExW")
+	regSetValueExW = advapi32.MustFindProc("RegSetValueExW")
+	regCloseKey    = advapi32.MustFindProc("RegCloseKey")
+	regDeleteValueW = advapi32.MustFindProc("RegDeleteValueW")
 )
 
 func SetAutostart(name string, exePath string) error {
 	var key syscall.Handle
-	ret, _, _ := regOpenKeyExW.Call(
+	r0, _, err := regOpenKeyExW.Call(
 		syscall.HKEY_CURRENT_USER,
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(regKeyPath))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(`Software\Microsoft\Windows\CurrentVersion\Run`))),
 		0,
 		syscall.KEY_WRITE,
 		uintptr(unsafe.Pointer(&key)),
 	)
-	if ret != 0 {
-		return fmt.Errorf("failed to open registry key: %d", ret)
+	if r0 != 0 {
+		return fmt.Errorf("failed to open registry key: %w", err)
 	}
 	defer regCloseKey.Call(uintptr(key))
 
-	exePathPtr := syscall.StringToUTF16Ptr(exePath)
-	ret, _, _ = regSetValueExW.Call(
+	r0, _, err = regSetValueExW.Call(
 		uintptr(key),
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(name))),
 		0,
 		syscall.REG_SZ,
-		uintptr(unsafe.Pointer(exePathPtr)),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(exePath))),
 		uintptr(len(exePath)*2),
 	)
-	if ret != 0 {
-		return fmt.Errorf("failed to set registry value: %d", ret)
+	if r0 != 0 {
+		return fmt.Errorf("failed to set registry value: %w", err)
 	}
 
 	return nil
@@ -49,24 +45,24 @@ func SetAutostart(name string, exePath string) error {
 
 func RemoveAutostart(name string) error {
 	var key syscall.Handle
-	ret, _, _ := regOpenKeyExW.Call(
+	r0, _, err := regOpenKeyExW.Call(
 		syscall.HKEY_CURRENT_USER,
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(regKeyPath))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(`Software\Microsoft\Windows\CurrentVersion\Run`))),
 		0,
 		syscall.KEY_WRITE,
 		uintptr(unsafe.Pointer(&key)),
 	)
-	if ret != 0 {
-		return fmt.Errorf("failed to open registry key: %d", ret)
+	if r0 != 0 {
+		return fmt.Errorf("failed to open registry key: %w", err)
 	}
 	defer regCloseKey.Call(uintptr(key))
 
-	ret, _, _ = syscall.NewLazyDLL("advapi32.dll").NewProc("RegDeleteValueW").Call(
+	r0, _, err = regDeleteValueW.Call(
 		uintptr(key),
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(name))),
 	)
-	if ret != 0 && ret != 2 {
-		return fmt.Errorf("failed to delete registry value: %d", ret)
+	if r0 != 0 && r0 != 2 {
+		return fmt.Errorf("failed to delete registry value: %w", err)
 	}
 
 	return nil
