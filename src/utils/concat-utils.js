e20ee344b4524074b7c18586acd8b811ee131db1
qhsmhq <qin__xuan@yeah.net>
2026-05-27 15:46:32 +0800

    feat(concat): add buildNConcatFormula for N-column support

--- src/utils/concat-utils.js
+++ src/utils/concat-utils.js
@@ -22,4 +22,31 @@ function buildConcatFormula(firstColLetter, secondColLetter, connector) {
   );
 }
 
-module.exports = { getColumnLetter, escapeFormulaText, buildConcatFormula };
+/**
+ * Build concatenation formula for N columns
+ * @param {string[]} columns - Array of column letters, e.g. ['A', 'B', 'C']
+ * @param {string} connector - Connector string, e.g. '_'
+ * @returns {string} Excel formula string
+ */
+function buildNConcatFormula(columns, connector) {
+  var escaped = escapeFormulaText(connector);
+  var rowRef = "1";
+
+  // Build empty-check string: "A1&B1&C1"
+  var emptyCheck = columns.map(function(col) {
+    return col + rowRef;
+  }).join('&');
+
+  // Build concat string: 'A1&"_"&B1&"_"&C1'
+  var concatParts = columns.map(function(col, index) {
+    if (index === 0) {
+      return col + rowRef;
+    }
+    return '"' + escaped + '"&' + col + rowRef;
+  });
+  var concatStr = concatParts.join('&');
+
+  return '=IF(' + emptyCheck + '="","",' + concatStr + ')';
+}
+
+module.exports = { getColumnLetter, escapeFormulaText, buildConcatFormula, buildNConcatFormula };
